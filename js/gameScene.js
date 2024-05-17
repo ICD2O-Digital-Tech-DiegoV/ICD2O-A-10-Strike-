ed1424c34bdebd6754dd3e1096bfcb3f8fa1520c
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-05-17 00:31:52 +0000

    edit-#15

--- js/gameScene.js
+++ js/gameScene.js
@@ -17,14 +17,24 @@ class GameScene extends Phaser.Scene {
     setTimeout(()=>{this.debounce=false},600)
   }
   
-  
+  // sound of Gun, missles and explosion
+  this.load.audio('Gun', '././assets/explosion.mp3')
+  this.load.audio('Missles', '././assets/explosion.mp3')
+  this.load.audio('explosion', '././assets/explosion.mp3')
 
+  //score set up
+  this.scoreText = this.add.text(10, 10, 'Score: 0' + this.score.toString(), this.scoreTextStyle)
+  
   constructor() {
     super({ key: 'gameScene' })
-    this.background = null
     this.ship = null
-    this.fireMissile = false
-    this.fireGun = false
+    this.fireguns = false
+    this.firemissile = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = {font: '65px Arial', fill: '#ffffff', align: 'center'}
+    this.gameOverTextStyle = {font: '65px Arial', fill: '#ff0000', align: 'center'}
+    
   }
   //color
   init(data) {
@@ -139,6 +149,39 @@ class GameScene extends Phaser.Scene {
     // create agroup fo the tanks
     this.tankGroup = this.add.group();
     this.createTank();
+
+    // colisions between projectiles and tanks
+    this.physics.add.collider(this.missleGroup, this.tankGroup, function (missileCollide, tankCollide){
+      tankCollide.destroy()
+      missileCollide.desroy()
+      this.sound.play('explosion')
+      this.score = this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
+      this.createTank()
+      this.createTank()
+      }.bind(this))
+
+    // colisions between projectiles and tanks
+    this.physics.add.collider(this.gunGroup, this.tankGroup, function (gunCollide, tankCollide){
+      tankCollide.destroy()
+      gunCollide.desroy()
+      this.sound.play('explosion')
+      this.score = this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
+      this.createTank()
+      this.createTank()
+      }.bind(this))
+
+  // colissions between the A-10 and the tanks
+    this.physics.add.collider(this.A10, this.tankGroup, function (A10collide, tankCollide){
+      this.sound.play('explosion')
+      this.physics.pause();
+      tankCollide.destroy()
+      A10collide.destroy()
+      this.gameOverText = this.add.text(400, 300, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)       
+      this.gameOverText.setInteractive({ useHandCursor: true })
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+     }.bind(this))
   }
 }
 
